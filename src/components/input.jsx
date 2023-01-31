af8d3e5b9be8a524e58dff03e1df5de6a524e536
AnnaS <53278654+AniaSelezneva@users.noreply.github.com>
2023-01-31 03:12:55 +0300

    first commit

---
+++ src/components/input.jsx
@@ -0,0 +1,52 @@
+import React, { useState, useRef } from "react"
+import * as styles from "../styles.module.scss"
+
+function Input({ setQuestionAsked }) {
+  const [question, setQuestion] = useState("")
+  const [savedQuestion, setSavedQuestion] = useState("")
+  const input = useRef()
+  const [questionSent, setQuestionSent] = useState(false)
+
+  return (
+    <form className={styles.form}>
+      <div>
+        <label htmlFor="name">Your YES/NO question: </label>
+        <input
+          ref={input}
+          value={question}
+          type="text"
+          name="name"
+          id="name"
+          required
+          onChange={e => {
+            setQuestion(e.target.value)
+          }}
+        ></input>
+        <button
+          onClick={async e => {
+            e.preventDefault()
+
+            if (question.trim().length > 0) {
+              setQuestionAsked(true)
+              setQuestionSent(true)
+              setSavedQuestion(question)
+              setQuestion("")
+              try {
+                await sendQuestionToServer(question)
+              } catch (error) {
+                console.log(error)
+              }
+            }
+          }}
+        >
+          Ask
+        </button>
+        <p style={{ visibility: questionSent ? "visible" : "hidden" }}>
+          Your question was: {savedQuestion}
+        </p>
+      </div>
+    </form>
+  )
+}
+
+export default Input
